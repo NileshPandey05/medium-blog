f05f28346fdd7c4f93e52b22ebbbbf72face0a03
Nilesh Pandey <nileshpandey_ce_2021@ltce.in>
2025-04-14 02:50:41 +0530

    Quote component

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,13 @@
+export default function Quote(){
+    return(
+        <div className="bg-slate-300 h-screen flex flex-col justify-center items-center space-y-3 ">
+            <div className="max-w-lg text-center font-serif font-medium text-2xl">
+                " The customer service I reveived was exceptional. The support team went above and beyond to address my concerns. "
+            </div>
+            <div className="pl-0 text-left max-w-sm  font-bold font-serif">
+                Jules Winnfield
+            </div>
+            <div className="-mt-2.5 font-serif font-medium">CEO, Acme Inc</div>
+        </div>
+    )
+}
