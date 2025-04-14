b13cd483ef8df500602cc494824607631732445c
Nilesh Pandey <nileshpandey_ce_2021@ltce.in>
2025-04-14 21:27:01 +0530

    other part of component

---
+++ frontend/src/components/BlogCard.tsx
@@ -0,0 +1,13 @@
+interface BlogCardProps {
+    autherName: string;
+    titile: string,
+    content: string
+}
+
+export default function BlogCard(){
+    return(
+        <div>
+
+        </div>
+    )
+}
